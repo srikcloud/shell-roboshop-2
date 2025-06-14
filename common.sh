5e6238b64664a30054dc461a52064e67f55c52e2
srikanth.baireddy <srikanth.baireddy@outlook.com>
2025-06-15 00:32:56 +0530

    Hello World Started

--- common.sh
+++ common.sh
@@ -13,6 +13,53 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+
+app_setup(){
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading $app_name"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/$app_name.zip &>>$LOG_FILE
+VALIDATE $? "unzipping $app_name"
+}
+
+nodejs_setup(){
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs:20"
+
+systemd_setup(){
+     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+VALIDATE $? "Copying $app_name service"
+
+systemctl daemon-reload &>>$LOG_FILE
+systemctl enable $app_name  &>>$LOG_FILE
+systemctl start $app_name
+VALIDATE $? "Starting $app_name"
+
+}
+
+
+}
 check_root(){
 #check the user has root previleges or not
 if [ $USERID -ne 0 ]
